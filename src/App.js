31dc6dd16499fc87b4c6f325622f8670b1d86c8c
Seif Ashraf <seif_fire2@hotmail.com>
2022-09-28 01:16:31 +0200

    feat: implemented delete functionallity, now can only type certain amount of digits

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import './App.css';
 import Input from './components/Input';
 import Screen from './components/Screen';
 import TopSection from './components/TopSection';
-
+import { useState } from 'react'
 
 function App() {
+  const MAX_DIGITS = 5
+  const [currInput, setCurrInput]= useState('')
+
+  const updateScreen = (newInput) => {
+    if(currInput.length < MAX_DIGITS)
+      setCurrInput(currInput + newInput)
+  }
+
+  const deleteNum = () => {
+    if(currInput.length > 0) {
+      const newArr = currInput.split('').filter((num, i ) => 
+        i != currInput.length - 1
+      ).join('');
+      console.log(newArr)
+      setCurrInput(newArr);
+    }
+  }
+
   return (
     <div className='page-wrapper m-container'>
       <TopSection />
-      <Screen />
-      <Input />
+      <Screen input={currInput} updateInput={updateScreen} />
+      <Input onNumClicked={updateScreen} onDeleteClicked={deleteNum}/>
     </div>
   );
 }
